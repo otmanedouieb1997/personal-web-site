f891869551d47fbbfa7b2a48d560e437064256c3
otmanedouieb1997 <otmane.douieb.1997@hotmail.com>
2022-01-12 23:40:56 +0100

    start convers static data to json file in folder data

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const ejs = require('ejs')
+const fs = require('fs')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+const data = () => {
+    let data = fs.readFileSync('./data/data.json','utf-8')
+    data = JSON.parse(data)
+    console.log(data[0].menu.length)
+    return data
+}
+
 app.get('/', (req, res) => {
-    res.render('home')
+    let data1 = data()
+    res.render('home',{data1})
 })
 
 app.listen(3000, () => {
